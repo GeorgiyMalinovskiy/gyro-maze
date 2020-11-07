ee2a7762a0cd857b12b845091d37090eab2d7253
Georgiy Malinovskiy <post@gmalinovskiy.su>
2020-11-07 11:33:52 +0300

    Add basic geometry and cam

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -10,10 +10,13 @@ public class GameManager : MonoBehaviour
     private Vector3 planeRotation;
     private GameObject plane;
     private int planeSize = 5;
+    
+    private GameObject checkPlane;
 
     void Awake() {
         cam = (Camera)GameObject.FindObjectOfType(typeof(Camera));
         plane = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        checkPlane = GameObject.CreatePrimitive(PrimitiveType.Cube);
     }
 
     void Start()
@@ -21,6 +24,21 @@ public class GameManager : MonoBehaviour
         cam.transform.position = plane.transform.position + new Vector3(-planeSize, 3, -planeSize);
         cam.transform.LookAt(plane.transform);
         plane.transform.localScale = new Vector3(planeSize, .25f, planeSize);
+
+        checkPlane.transform.localScale = new Vector3(
+            Vector3.Magnitude(plane.transform.position - cam.transform.position),
+            .2f,
+            .2f
+        );
+
+        Vector3 camDirection = Vector3.Normalize(cam.transform.position);
+        Vector3 planeDirection = Vector3.Normalize(plane.transform.position);
+        float rotationAngle = (float)Mathf.Acos(Vector3.Dot(camDirection, planeDirection));
+        Vector3 rotationAxis = Vector3.Cross(cam.transform.position, plane.transform.position);
+        Debug.Log(cam.transform.rotation);
+        Debug.Log(plane.transform.rotation);
+
+        checkPlane.transform.Rotate(rotationAxis);
 
         if (Gamepad.current == null) {
             Debug.Log("Gamepad not connected");
